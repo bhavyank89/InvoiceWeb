a6c568679d5cd4a0307e495cb87b914c4f4f385b
bhavyank89 <bhavyanksarolia@gmail.com>
2025-04-03 11:13:13 +0530

    add TableForm in invoiceForm

--- src/components/InvoiceForm.jsx
+++ src/components/InvoiceForm.jsx
@@ -4,14 +4,14 @@ export default function Form({ notes, name, address, clientName, clientAddress, 
   return (
     <>
       {/* Invoice Form */}
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center drop-shadow-md">
 
         {/* Personal Information Section */}
         <section className="flex flex-col mb-8 justify-center item-center md:grid md:grid-cols-2 md:justify-around">
           <label className="font-bold w-[100%] mb-2 text-left col-span-2" htmlFor="name">Personal Information</label>
           {/* name */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="name">Name</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="name">Name<span className="text-red-500">*</span></label>
             <input
               className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto md:mr-5"
@@ -26,31 +26,31 @@ export default function Form({ notes, name, address, clientName, clientAddress, 
           </div>
           {/* email */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="email">Email</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="email">Email<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto" type="email" name="email" id="email" placeholder="Enter your email" autoComplete="off" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           {/* phone */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="phone">Phone</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="phone">Phone<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto md:mr-5" type="text" name="phone" id="phone" placeholder="Enter your phone" autoComplete="off" value={phone} onChange={(e) => setPhone(e.target.value)} />
           </div>
           {/* GST No */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="personalGST">GST No</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="personalGST">GST No<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto" type="text" name="personalGST" id="personalGST" placeholder="Enter your GST" autoComplete="off" value={personalGST} onChange={(e) => setPersonalGST(e.target.value)} />
           </div>
           {/* website */}
           <div className="flex flex-col col-span-2">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="website">Website</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="website">Website<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto" type="text" name="website" id="website" placeholder="Enter your website" autoComplete="off" value={website} onChange={(e) => setWebsite(e.target.value)} />
           </div>
           {/* Address */}
           <div className="flex flex-col md:col-span-2">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="address">Address</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="address">Address<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto" type="text" name="address" id="address" placeholder="Enter your address" autoComplete="off" value={address} onChange={(e) => setAddress(e.target.value)} />
           </div>
@@ -61,25 +61,25 @@ export default function Form({ notes, name, address, clientName, clientAddress, 
           <label className="font-bold w-[100%] mb-2 text-left col-span-2" htmlFor="name">Bank Details</label>
           {/* bankName */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="bankName">Bank Name</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="bankName">Bank Name<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto md:mr-5" type="text" name="bankName" id="bankName" placeholder="Enter your bankName" autoComplete="off" value={bankName} onChange={(e) => setBankName(e.target.value)} />
           </div>
           {/* bank Account Number */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="bankAccount">Enter Account No</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="bankAccount">Enter Account No<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto" type="text" name="bankAccount" id="bankAccount" placeholder="Account No" autoComplete="off" value={bankAccount} onChange={(e) => setBankAccount(e.target.value)} />
           </div>
           {/* IFSC Code */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="bankIFSC">IFSC Code</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="bankIFSC">IFSC Code<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto md:mr-5" type="text" name="bankIFSC" id="bankIFSC" placeholder="Enter your IFSC" autoComplete="off" value={bankIFSC} onChange={(e) => setBankIFSC(e.target.value)} />
           </div>
           {/*Branch Name*/}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="branchName">Branch Name</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="branchName">Branch Name<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto" type="text" name="branchName" id="branchName" placeholder="Enter your branchName" autoComplete="off" value={branchName} onChange={(e) => setBranchName(e.target.value)} />
           </div>
@@ -90,19 +90,19 @@ export default function Form({ notes, name, address, clientName, clientAddress, 
           <label className="font-bold w-[100%] mb-2 text-left col-span-2" htmlFor="name">Client Information</label>
           {/* clientName */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="clientName">Client Name</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="clientName">Client Name<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto md:mr-5" type="text" name="clientName" id="clientName" placeholder="ClientName" autoComplete="off" value={clientName} onChange={(e) => setClientName(e.target.value)} />
           </div>
           {/* GST No */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="clientGST">GST No</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="clientGST">GST No<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto" type="text" name="clientGST" id="clientGST" placeholder="clientGST" autoComplete="off" value={clientGST} onChange={(e) => setClientGST(e.target.value)} />
           </div>
           {/* clientAddress */}
           <div className="flex flex-col md:col-span-2">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="clientAddress">Client Address</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="clientAddress">Client Address<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto" type="clientAddress" name="clientAddress" id="clientAddress" placeholder="Enter your clientAddress" autoComplete="off" value={clientAddress} onChange={(e) => setClientAddress(e.target.value)} />
           </div>
@@ -113,19 +113,19 @@ export default function Form({ notes, name, address, clientName, clientAddress, 
           <label className="font-bold w-[100%] mb-2 text-left col-span-3" htmlFor="name">Invoice Details</label>
           {/* invoiceNumber */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="invoiceNumber">Invoice Number</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="invoiceNumber">Invoice Number<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto md:mr-5" type="text" name="invoiceNumber" id="invoiceNumber" placeholder="Enter your invoiceNumber" autoComplete="off" value={invoiceNumber} onChange={(e) => setInvoiceNumber(e.target.value)} />
           </div>
           {/* invoiceDate */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="invoiceDate">Invoice Date</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="invoiceDate">Invoice Date<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto md:mr-5" type="date" name="invoiceDate" id="invoiceDate" placeholder="Enter invoiceDate" autoComplete="off" value={invoiceDate} onChange={(e) => setInvoiceDate(e.target.value)} />
           </div>
           {/* dueDate */}
           <div className="flex flex-col">
-            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="dueDate">Invoice due date</label>
+            <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="dueDate">Invoice due date<span className="text-red-500">*</span></label>
             <input className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
              hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto" type="date" name="dueDate" id="dueDate" placeholder="Enter your dueDate" autoComplete="off" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
           </div>
@@ -134,14 +134,17 @@ export default function Form({ notes, name, address, clientName, clientAddress, 
         {/* Item Tables */}
         <section className="flex flex-col justify-center item-center mb-8 md:flex-row md:flex-wrap md:justify-around">
           <label className="font-bold w-[100%] mb-2 text-left" htmlFor="name">Item Details</label>
-          <TableForm quantity={quantity}
-            setQuantity={setQuantity}
-            amount={amount}
-            setAmount={setAmount}
-            price={price}
-            setPrice={setPrice}
-            description={description}
-            setDescription={setDescription} />
+          <div className="w-[100%]">
+
+            <TableForm quantity={quantity}
+              setQuantity={setQuantity}
+              amount={amount}
+              setAmount={setAmount}
+              price={price}
+              setPrice={setPrice}
+              description={description}
+              setDescription={setDescription} />
+          </div>
         </section>
 
         {/* Additional Notes Section */}
--- src/components/TableForm.jsx
+++ src/components/TableForm.jsx
@@ -1,14 +1,66 @@
-export default function TableForm({ quantity,
-    setQuantity,
-    amount,
-    setAmount,
-    price,
-    setPrice,
-    description,
-    setDescription }){
+import { useEffect } from "react"
+
+export default function TableForm({ quantity, setQuantity, amount, setAmount, price, setPrice, description, setDescription, }) {
+    useEffect(() => {
+        setAmount(quantity * price);
+    }, [price, quantity]);
     return (
-        <>
-        
-        </>
+        <section className="flex flex-col md:grid grid-cols-3 border-black-100 border-1 rounded-xl shadow-lg p-5">
+            <div className="flex flex-col md:col-span-3">
+                <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="name">Description<span className="text-red-500">*</span></label>
+                <input
+                    className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
+             hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto"
+                    type="text"
+                    name="description"
+                    id="description"
+                    placeholder="dscription"
+                    autoComplete="off"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+            </div>
+            <div className="flex flex-col md:col-span-1">
+                <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="name">Price<span className="text-red-500">*</span></label>
+                <input
+                    className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
+             hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto md:mr-5 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                    type="number"
+                    name="price"
+                    id="price"
+                    placeholder="price"
+                    autoComplete="off"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                />
+            </div>
+            <div className="flex flex-col md:col-span-1">
+                <label className="font-bold text-sm mx-1 mb-0.5" htmlFor="name">Quantity<span className="text-red-500">*</span></label>
+                <input
+                    className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
+             hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto md:mr-5 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                    type="number"
+                    name="quantity"
+                    id="quantity"
+                    placeholder="quantity"
+                    autoComplete="off"
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                />
+            </div>
+            <div className="flex flex-col md:col-span-1">
+                <label className="font-bold number-sm mx-1 mb-0.5" htmlFor="name">Amount</label>
+                <input
+                    className="border border-gray-300 p-2 rounded h-10 mb-2 transition-all duration-100 
+             hover:border-blue-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none md:w-auto cursor-not-allowed"
+                    type="text"
+                    name="amount"
+                    id="amount"
+                    placeholder="amount"
+                    autoComplete="off"
+                    value={amount}
+                />
+            </div>
+        </section>
     )
 }
