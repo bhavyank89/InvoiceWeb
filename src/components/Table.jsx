562402075e4a24b979991ca9840cb9d70ca39feb
bhavyank89 <bhavyanksarolia@gmail.com>
2025-04-03 08:16:11 +0530

    add Table.jsx

---
+++ src/components/Table.jsx
@@ -0,0 +1,44 @@
+export default function Table({ quantity,
+    setQuantity,
+    amount,
+    setAmount,
+    price,
+    setPrice,
+    description,
+    setDescription }) {
+    return (
+        <>
+            {/* Table */}
+            <table width="100%">
+                <thead>
+                    <tr className="bg-blue-50">
+                        <th>Description</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                        <td>Malcolm Lockyer</td>
+                        <td>1961</td>
+                        <td>1961</td>
+                    </tr>
+                    <tr>
+                        <td>Witchy Woman</td>
+                        <td>The Eagles</td>
+                        <td>1972</td>
+                        <td>1972</td>
+                    </tr>
+                    <tr>
+                        <td>Shining Star</td>
+                        <td>Earth, Wind, and Fire</td>
+                        <td>1975</td>
+                    </tr>
+                </tbody>
+            </table>
+            {/* End of table */}
+        </>
+    )
+}
